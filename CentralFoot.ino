9c6d6ff712abf26249e82552c6d13d40f2810e0e
joeakselrod <akselrodjr@gmail.com>
2021-03-26 20:11:12 -0400

    No more helper functions
    maybe it works, idk

--- CentralFoot.ino
+++ CentralFoot.ino
@@ -1,5 +1,5 @@
-//Joseph Akselrod rev 0.2.6
-//3-23-2021
+//Joseph Akselrod rev 0.2.7
+//3-26-2021
 //Central device
 
 #include <ArduinoBLE.h>
@@ -14,8 +14,9 @@ const char right_foot_uuid[] = "f71ffde7-c9da-434d-9cc9-eb25834cb67a";
 const char right_stream_uuid[] = "55e46e53-d546-49ee-be72-a2cbd47992f1";
 const char right_calibration_uuid[] = "88c51252-497b-46b7-80fd-926865f6169c ";
 
-BLECharCharacteristic leftStream = NULL;
-BLECharCharacteristic rightStream = NULL;
+//Create BLEDevice objects for peripherals
+BLEDevice leftFoot = BLE.peripheral();
+BLEDevice rightFoot = BLE.peripheral();
 
 
 //Create BLE attribure profile (ATT)for central
@@ -23,58 +24,6 @@ BLECharCharacteristic rightStream = NULL;
 //assign characteristics for corresponding services
 
 
-
-//Helper functions------------------------------------------------------------------------------------
-
-//getPeriph() subscribes to a BLE service such as "leftStream" allows for automatic notification from peripheral to central
-void getPeriph(BLEDevice peripheral, char *serviceUuid[], char *streamUuid, char *calibrateUuid, BLECharCharacteristic *stream, BLECharCharacteristic *calibrate)
-{
-  // check if a peripheral has been discovered
-  BLEDevice peripheral = BLE.available();
-
-  if (peripheral) {
-    // discovered a peripheral, print out address, local name, and advertised service
-    Serial.print("Found ");
-    Serial.print(peripheral.address());
-    Serial.print(" '");
-    Serial.print(peripheral.localName());
-    Serial.print("' ");
-    Serial.print(peripheral.advertisedServiceUuid());
-    Serial.println();
-
-    // see if peripheral is a left or right based on the service uuid
-    if (strcmp(peripheral.advertisedServiceUuid(), serviceUuid)==0)
-    {
-      // stop scanning
-      BLE.stopScan();
-      //subscribe to the characteristics of the peripheral
-      BLECharCharacteristic calibrate = peripheral.characteristic(calibrateUuid);
-      BLECharCharacteristic stream = peripheral.characteristic(streamUuid);
-        // subscribe to the simple key characteristic
-    Serial.println("Subscribing to calibrate characteristic ...");
-     if (!simpleKeyCharacteristic) {
-      Serial.println("no calibrate characteristic found!");
-      peripheral.disconnect();
-      return;
-      } else if (!simpleKeyCharacteristic.canSubscribe()) {
-    Serial.println("calibration characteristic is not subscribable!");
-    peripheral.disconnect();
-    return;
-  } else if (!simpleKeyCharacteristic.subscribe()) {
-    Serial.println("subscription failed!");
-    peripheral.disconnect();
-    return;
-  } else {
-    Serial.println("Subscribed");
-  }
-      // peripheral disconnected, we are done
-      while (1)
-      {
-        // do nothing
-      }
-   }
-  }
-}
 
 void setup()
 {
@@ -88,13 +37,190 @@ void setup()
     while (1);
   }
   Serial.println("BLE Central - Looking for peripherals...");
-  // start scanning for peripherals
-  BLE.scan();
-  //get correct peripheral and subscribe to its characteristics
-  getPeriph(leftFoot, left_foot_uuid, left_stream_uuid, left_calibration_uuid, leftStream, leftCalibrate);
-  BLE.scan();
-  getPeriph(rightFoot, right_foot_uuid, right_stream_uuid, right_calibration_uuid), rightStream, rightCalibrate;
+  // start scanning for left foot peripheral by uuid
+  BLE.scanForUuid("4b072a8c-447a-4552-a49f-3fc072368892");
 
+    // check if a peripheral has been discovered
+    BLEDevice leftFoot = BLE.available();
+
+    if (leftFoot) {
+      // discovered a peripheral, print out address, local name, and advertised service
+      Serial.print("Found ");
+      Serial.print(leftFoot.address());
+      Serial.print(" '");
+      Serial.print(leftFoot.localName());
+      Serial.print("' ");
+      Serial.print(leftFoot.advertisedServiceUuid());
+      Serial.println();
+
+      // see if peripheral is the one we are looking for based on uuid
+      if (strcmp(leftFoot.advertisedServiceUuid(), "4b072a8c-447a-4552-a49f-3fc072368892") == 0)
+      {
+        // stop scanning
+        BLE.stopScan();
+        //subscribe to the characteristics of the peripheral
+        BLECharCharacteristic leftCalibrate = leftFoot.characteristic("6532b055-f7ae-4a16-b551-93b58af62518");
+
+          // subscribe to the simple key characteristic
+        Serial.println("Subscribing to calibrate characteristic ...");
+        if (!leftCalibrate)
+        {
+          Serial.println("no calibrate characteristic found!");
+          leftFoot.disconnect();
+          return;
+        }
+        else if (!leftCalibrate.canSubscribe())
+        {
+          Serial.println("calibration characteristic is not subscribable!");
+          leftFoot.disconnect();
+          return;
+        }
+        else if (!leftCalibrate.subscribe())
+        {
+          Serial.println("subscription failed!");
+          leftFoot.disconnect();
+          return;
+        }
+        else
+        {
+          Serial.println("Subscribed");
+        }
+        // peripheral disconnected, we are done
+        while (1)
+        {
+          // do nothing
+        }
+
+
+        //Now We subscribe to the left foot stream characteristic
+
+        BLECharCharacteristic leftStream = leftFoot.characteristic("876e1475-1fec-4a95-be64-d1b634f35511");
+
+          // subscribe to the simple key characteristic
+        Serial.println("Subscribing to stream characteristic ...");
+        if (!leftStream)
+        {
+          Serial.println("no stream characteristic found!");
+          leftFoot.disconnect();
+          return;
+        }
+        else if (!leftStream.canSubscribe())
+        {
+          Serial.println("stream characteristic is not subscribable!");
+          leftFoot.disconnect();
+          return;
+        }
+        else if (!leftStream.subscribe())
+        {
+          Serial.println("subscription failed!");
+          leftFoot.disconnect();
+          return;
+        }
+        else
+        {
+          Serial.println("Subscribed");
+        }
+        // peripheral disconnected, we are done
+        while (1)
+        {
+          // do nothing
+        }
+     }
+    }
+
+
+
+    //////////////////////////////////////////////////////////////////////////
+    // start scanning for right foot peripheral by uuid
+    //////////////////////////////////////////////////////////////////////////
+
+    //right foot service
+    BLE.scanForUuid("f71ffde7-c9da-434d-9cc9-eb25834cb67a");
+
+      // check if a peripheral has been discovered
+      BLEDevice rightFoot = BLE.available();
+
+      if (rightFoot) {
+        // discovered a peripheral, print out address, local name, and advertised service
+        Serial.print("Found ");
+        Serial.print(rightFoot.address());
+        Serial.print(" '");
+        Serial.print(rightFoot.localName());
+        Serial.print("' ");
+        Serial.print(rightFoot.advertisedServiceUuid());
+        Serial.println();
+
+        // see if peripheral is the one we are looking for based on uuid
+        if (strcmp(rightFoot.advertisedServiceUuid(), "f71ffde7-c9da-434d-9cc9-eb25834cb67a") == 0)
+        {
+          // stop scanning
+          BLE.stopScan();
+          //subscribe to the characteristics of the peripheral
+          BLECharCharacteristic rightCalibrate = rightFoot.characteristic("88c51252-497b-46b7-80fd-926865f6169c");
+
+            // subscribe to the simple key characteristic
+          Serial.println("Subscribing to calibrate characteristic ...");
+          if (!rightCalibrate)
+          {
+            Serial.println("no calibrate characteristic found!");
+            rightFoot.disconnect();
+            return;
+          }
+          else if (!rightCalibrate.canSubscribe())
+          {
+            Serial.println("calibration characteristic is not subscribable!");
+            rightFoot.disconnect();
+            return;
+          }
+          else if (!rightCalibrate.subscribe())
+          {
+            Serial.println("subscription failed!");
+            rightFoot.disconnect();
+            return;
+          }
+          else
+          {
+            Serial.println("Subscribed");
+          }
+          // peripheral disconnected, we are done
+          while (1)
+          {
+            // do nothing
+          }
+          //////////////////////////////////////////////////////
+          BLECharCharacteristic rightStream = rightFoot.characteristic("55e46e53-d546-49ee-be72-a2cbd47992f1");
+
+            // subscribe to the simple key characteristic
+          Serial.println("Subscribing to stream characteristic ...");
+          if (!rightStream)
+          {
+            Serial.println("no stream characteristic found!");
+            rightFoot.disconnect();
+            return;
+          }
+          else if (!rightStream.canSubscribe())
+          {
+            Serial.println("stream characteristic is not subscribable!");
+            rightFoot.disconnect();
+            return;
+          }
+          else if (!rightStream.subscribe())
+          {
+            Serial.println("subscription failed!");
+            rightFoot.disconnect();
+            return;
+          }
+          else
+          {
+            Serial.println("Subscribed");
+          }
+          // peripheral disconnected, we are done
+          while (1)
+          {
+            // do nothing
+          }
+       }
+      }
 }
 
 
@@ -103,7 +229,7 @@ void loop()
 {
   //calibrate data once
 Serial.println(leftCalibrate);
-Serial.println(leftCalibrate);
+Serial.println(rightCalibrate);
 
   while(1)
   {
